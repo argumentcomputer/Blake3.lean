63bc6aa89ac94295874bf7c2945d1e08972d33d6
Anders Christiansen Sørby <anders@sorby.xyz>
2021-10-11 17:27:16 +0200

    Update flake deps and CI

--- flake.nix
+++ flake.nix
@@ -4,9 +4,10 @@
   inputs = {
     lean = {
       url = github:leanprover/lean4;
+      # inputs.flake-utils.follows = "flake-utils";
     };
 
-    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-21.05;
     flake-utils = {
       url = github:numtide/flake-utils;
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,32 +16,42 @@
     blake3.url = github:yatima-inc/BLAKE3/acs/add-flake-setup;
   };
 
-  outputs = { self, lean, flake-utils, nixpkgs, blake3 }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, lean, flake-utils, nixpkgs, blake3 }:
     let
-      leanPkgs = lean.packages.${system};
-      blake3-c = blake3.packages.${system}.BLAKE3-c;
-      pkgs = import nixpkgs { inherit system; };
-      name = "Blake3";
-      project = leanPkgs.buildLeanPackage {
-        inherit name;
-        src = ./src;
-        debug = true;
-        linkFlags = [ blake3-c ];
-        staticLibDeps = [ blake3-c ];
-      };
+      supportedSystems = [
+        # "aarch64-linux"
+        # "aarch64-darwin"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
     in
-    {
-      inherit project;
-      packages = {
-        inherit (project) modRoot Blake3;
-        inherit (leanPkgs) lean;
-      };
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        leanPkgs = lean.packages.${system};
+        blake3-c = blake3.packages.${system}.BLAKE3-c;
+        pkgs = import nixpkgs { inherit system; };
+        name = "Blake3";
+        project = leanPkgs.buildLeanPackage {
+          inherit name;
+          src = ./src;
+          debug = true;
+          linkFlags = [ blake3-c ];
+          staticLibDeps = [ blake3-c ];
+        };
+      in
+      {
+        inherit project;
+        packages = {
+          inherit (project) modRoot Blake3;
+          inherit (leanPkgs) lean;
+        };
 
-      defaultPackage = project.modRoot;
-      devShell = pkgs.mkShell {
-        buildInputs = [ leanPkgs.lean ];
-        LEAN_PATH = "${leanPkgs.Lean.modRoot}";
-        CPATH = "${leanPkgs.Lean.modRoot}";
-      };
-    });
+        defaultPackage = project.modRoot;
+        devShell = pkgs.mkShell {
+          buildInputs = [ leanPkgs.lean ];
+          LEAN_PATH = "${leanPkgs.Lean.modRoot}";
+          CPATH = "${leanPkgs.Lean.modRoot}";
+        };
+      });
 }
