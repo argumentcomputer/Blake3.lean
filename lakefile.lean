0c00e4c6ff0870511661cf55f77cb499b6e2e316
Arthur Paulino <arthurleonardo.ap@gmail.com>
2025-02-20 08:55:59 -0300

    chore: several improvements
    Closes #3

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,8 @@ package Blake3
 
 lean_lib Blake3
 
+lean_exe Blake3Test
+
 target cloneBlake3 pkg : GitRepo := do
   let repoDir : GitRepo := pkg.dir / "blake3"
   let alreadyCloned ← repoDir.dir.pathExists
@@ -16,17 +18,19 @@ target cloneBlake3 pkg : GitRepo := do
 def blake3CDir (blake3Repo : GitRepo) : System.FilePath :=
   blake3Repo.dir / "c"
 
-opaque BLAKE3_FLAGS : Array String :=
+abbrev blake3Flags : Array String :=
   #["-DBLAKE3_NO_SSE2", "-DBLAKE3_NO_SSE41", "-DBLAKE3_NO_AVX2", "-DBLAKE3_NO_AVX512"]
 
-def buildBlake3Obj (pkg : NPackage _package.name) (fileName : String) (addFlags : Bool) := do
+abbrev compiler := "cc"
+
+def buildBlake3Obj (pkg : Package) (fileName : String) (addFlags : Bool) := do
   let blake3Repo ← cloneBlake3.fetch >>= (·.await)
   let cDir := blake3CDir blake3Repo
-  let srcJob ← inputTextFile $ cDir / (fileName ++ ".c")
-  let oFile := pkg.buildDir / (fileName ++ ".o")
+  let srcJob ← inputTextFile $ cDir / fileName |>.addExtension "c"
+  let oFile := pkg.buildDir / fileName |>.addExtension "o"
   let includeArgs := #["-I", cDir.toString]
-  let weakArgs := if addFlags then includeArgs ++ BLAKE3_FLAGS else includeArgs
-  buildO oFile srcJob weakArgs #["-fPIC"] "cc" getLeanTrace
+  let weakArgs := if addFlags then includeArgs ++ blake3Flags else includeArgs
+  buildO oFile srcJob weakArgs #[] compiler getLeanTrace
 
 target ffi.o pkg : System.FilePath := do
   let blake3Repo ← cloneBlake3.fetch >>= (·.await)
@@ -35,7 +39,7 @@ target ffi.o pkg : System.FilePath := do
   let includeDir ← getLeanIncludeDir
   let cDir := blake3CDir blake3Repo
   let weakArgs := #["-I", includeDir.toString, "-I", cDir.toString]
-  buildO oFile srcJob weakArgs #["-fPIC"] "cc" getLeanTrace
+  buildO oFile srcJob weakArgs #[] compiler getLeanTrace
 
 extern_lib ffi pkg := do
   let blake3O ← buildBlake3Obj pkg "blake3" false
