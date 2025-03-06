acd85941949f8a1fbe1fbed48441055a0a448456
Artem Storozhuk <storojs72@gmail.com>
2025-03-06 16:54:08 +0200

    feat: Expose custom Blake3 initialisers (#9)
    * feat: Expose keyed initializer
    
    * feat: Expose derive_key initializer
    
    * chore: lift key size guarantee to compile time (#10)
    
    Extra: minor fotmatting nits
    
    ---------
    
    Co-authored-by: Arthur Paulino <arthurleonardo.ap@gmail.com>

--- Blake3.lean
+++ Blake3.lean
@@ -2,11 +2,19 @@
 
 namespace Blake3
 
-/-- BLAKE3 output length -/
+/-- BLAKE3 constants -/
 abbrev BLAKE3_OUT_LEN : Nat := 32
+abbrev BLAKE3_KEY_LEN : Nat := 32
 
-/-- A dependent ByteArray which guarantees the correct byte length. -/
+/-- A wrapper around `ByteArray` whose size is `BLAKE3_OUT_LEN` -/
 def Blake3Hash : Type := { r : ByteArray // r.size = BLAKE3_OUT_LEN }
+
+/-- A wrapper around `ByteArray` whose size is `BLAKE3_KEY_LEN` -/
+def Blake3Key : Type := { r : ByteArray // r.size = BLAKE3_KEY_LEN }
+
+def Blake3Key.ofBytes (bytes : ByteArray)
+    (h : bytes.size = BLAKE3_KEY_LEN := by rw [ByteArray.size]; rfl) : Blake3Key :=
+  ⟨bytes, h⟩
 
 instance : Inhabited Blake3Hash where
   default := ⟨⟨#[
@@ -34,6 +42,13 @@ namespace Hasher
 @[extern "lean_blake3_init"]
 opaque init : Unit → Hasher
 
+/-- Initialize a hasher using pseudo-random key -/
+@[extern "lean_blake3_init_keyed"]
+opaque initKeyed (key : @& Blake3Key) : Hasher
+
+@[extern "lean_blake3_init_derive_key"]
+opaque initDeriveKey (context : @& ByteArray) : Hasher
+
 /-- Put more data into the hasher. This can be called several times. -/
 @[extern "lean_blake3_hasher_update"]
 opaque update (hasher : Hasher) (input : @& ByteArray) : Hasher
@@ -47,6 +62,26 @@ end Hasher
 /-- Hash a ByteArray -/
 def hash (input : ByteArray) : Blake3Hash :=
   let hasher := Hasher.init ()
+  let hasher := hasher.update input
+  let output := hasher.finalize BLAKE3_OUT_LEN.toUSize
+  if h : output.size = BLAKE3_OUT_LEN then
+    ⟨output, h⟩
+  else
+    panic! "Incorrect output size"
+
+/-- Hash a ByteArray using keyed initializer -/
+def hashKeyed (input : @& ByteArray) (key : @& Blake3Key) : Blake3Hash :=
+  let hasher := Hasher.initKeyed key
+  let hasher := hasher.update input
+  let output := hasher.finalize BLAKE3_OUT_LEN.toUSize
+  if h : output.size = BLAKE3_OUT_LEN then
+    ⟨output, h⟩
+  else
+    panic! "Incorrect output size"
+
+/-- Hash a ByteArray using initializer parameterized by some context -/
+def hashDeriveKey (input context : @& ByteArray) : Blake3Hash :=
+  let hasher := Hasher.initDeriveKey context
   let hasher := hasher.update input
   let output := hasher.finalize BLAKE3_OUT_LEN.toUSize
   if h : output.size = BLAKE3_OUT_LEN then
--- ffi.c
+++ ffi.c
@@ -53,6 +53,24 @@ extern lean_obj_res lean_blake3_init() {
 }
 
 /**
+ * Initialize a hasher using pseudo-random key
+ */
+extern lean_obj_res lean_blake3_init_keyed(b_lean_obj_arg key) {
+    blake3_hasher *a = malloc(sizeof(blake3_hasher));
+    blake3_hasher_init_keyed(a, lean_sarray_cptr(key));
+    return lean_alloc_external(get_blake3_hasher_class(), a);
+}
+
+/**
+ * Initialize a hasher using some arbitrary context
+ */
+ extern lean_obj_res lean_blake3_init_derive_key(b_lean_obj_arg context) {
+     blake3_hasher *a = malloc(sizeof(blake3_hasher));
+     blake3_hasher_init_derive_key_raw(a, lean_sarray_cptr(context), lean_sarray_size(context));
+     return lean_alloc_external(get_blake3_hasher_class(), a);
+ }
+
+/**
  * Ensure the hasher is exclusive.
  */
 static inline lean_obj_res lean_ensure_exclusive_blake3_hasher(lean_obj_arg a) {
